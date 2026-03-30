d846d85be5325addcfd44e959c4c0b0317671dcc
ThanhVuVu <oliverheldensfhm@gmail.com>
2026-03-30 11:10:19 +0700

    refactor: remove channel_attention module and its usage from ViT architecture

--- Trans.py
+++ Trans.py
@@ -117,70 +117,9 @@ class ClassificationHead(nn.Sequential):
         out = self.clshead(x)
         return x, out
 
-class channel_attention(nn.Module):
-    # Dimensions altered for 187 seq length and exactly 1 spatial channel
-    def __init__(self, sequence_num=187, inter=11):
-        super(channel_attention, self).__init__()
-        self.sequence_num = sequence_num
-        self.inter = inter
-        self.extract_sequence = int(self.sequence_num / self.inter)
-
-        # Depth scalars mapped perfectly to 1
-        self.query = nn.Sequential(
-            nn.Linear(1, 1),
-            nn.LayerNorm(1),
-            nn.Dropout(0.3)
-        )
-        self.key = nn.Sequential(
-            nn.Linear(1, 1),
-            nn.LayerNorm(1),
-            nn.Dropout(0.3)
-        )
-
-        self.projection = nn.Sequential(
-            nn.Linear(1, 1),
-            nn.LayerNorm(1),
-            nn.Dropout(0.3),
-        )
-
-        self.drop_out = nn.Dropout(0)
-        self.pooling = nn.AvgPool2d(kernel_size=(1, self.inter), stride=(1, self.inter))
-
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight)
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0.0)
-
-    def forward(self, x):
-        temp = rearrange(x, 'b o c s->b o s c')
-        temp_query = rearrange(self.query(temp), 'b o s c -> b o c s')
-        temp_key = rearrange(self.key(temp), 'b o s c -> b o c s')
-
-        channel_query = self.pooling(temp_query)
-        channel_key = self.pooling(temp_key)
-
-        scaling = self.extract_sequence ** (1 / 2)
-        channel_atten = torch.einsum('b o c s, b o m s -> b o c m', channel_query, channel_key) / scaling
-        channel_atten_score = F.softmax(channel_atten, dim=-1)
-        channel_atten_score = self.drop_out(channel_atten_score)
-
-        out = torch.einsum('b o c s, b o c m -> b o c s', x, channel_atten_score)
-        out = rearrange(out, 'b o c s -> b o s c')
-        out = self.projection(out)
-        out = rearrange(out, 'b o s c -> b o c s')
-        return out
-
 class ViT(nn.Sequential):
     def __init__(self, emb_size=10, depth=3, n_classes=5, **kwargs):
         super().__init__(
-            ResidualAdd(
-                nn.Sequential(
-                    nn.LayerNorm(187), # Core length parameter matched to 187
-                    channel_attention(),
-                    nn.Dropout(0.5),
-                )
-            ),
             PatchEmbedding(emb_size),
             TransformerEncoder(depth, emb_size),
             ClassificationHead(emb_size, n_classes)
